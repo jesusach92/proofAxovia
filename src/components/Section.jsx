b70e099e1351b4b90101a3e3dbbb6d5b286481c0
Jesus Alberto Castillo <jesusach92@gmail.com>
2022-07-22 00:31:23 -0500

    4to Commit

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { FilterBar } from "./FilterBar";
+
 import Widget from "./Widget";
 import axios from "axios";
-import stringifyObject from "stringify-object";
+import { CurrencyDollar } from "react-bootstrap-icons";
 
 const Section = () => {
   const [data, setData] = useState([
@@ -87,7 +87,88 @@ const Section = () => {
       size: "L",
     },
   ]);
-  const [dataFilter, setDataFilter] = useState({});
+  const [dataFilter, setDataFilter] = useState([
+    {
+      name: "pastel de 3 leches",
+      imageUrl: "https://axovia.mx/challenge/3leches.jpeg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a dictum lacus. Quisque in aliquet dui. Pellentesque mauris turpis, imperdiet ut viverra ut, porttitor sit amet felis. Ut viverra auctor quam in consectetur.",
+      price: "400.65232",
+      comboPrice: "500.2141",
+      toppingType: "0",
+      size: "M",
+    },
+    {
+      name: "pastel de queso con zarzamora",
+      imageUrl: "https://axovia.mx/challenge/pastel de zarzamora.jpeg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a dictum lacus. Quisque in aliquet dui. Pellentesque mauris turpis, imperdiet ut viverra ut, porttitor sit amet felis. Ut auctor quam in consectetur.",
+      price: "400.65232",
+      comboPrice: "500.43335",
+      toppingType: "1",
+      size: "S",
+    },
+    {
+      name: "pay de limon",
+      imageUrl: "https://axovia.mx/challenge/paydelimon.png",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a dictum lacus. Quisque in aliquet dui. Pellentesque mauris turpis, imperdiet ut viverra ut, porttitor sit amet felis. Ut viverra auctor quam in consectetur.",
+      price: "400.65232",
+      comboPrice: "400.11143214",
+      toppingType: "2",
+      size: "S",
+    },
+    {
+      name: "pastel que dice uwu",
+      imageUrl: "https://axovia.mx/challenge/uwu.jpeg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a lacus. Quisque in aliquet dui. Pellentesque mauris turpis, imperdiet ut viverra ut, porttitor sit amet felis. Ut viverra auctor quam in consectetur.",
+      price: "400.65232",
+      comboPrice: "100.50",
+      toppingType: "0",
+      size: "M",
+    },
+    {
+      name: "pastel de zanahoria",
+      imageUrl: "https://axovia.mx/challenge/pastelzanahoria.jpeg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a dictum lacus. Quisque in aliquet dui. Pellentesque mauris turpis, imperdiet ut viverra ut, porttitor sit amet felis. Ut viverra auctor quam in consectetur.",
+      price: "400.65232",
+      comboPrice: "1300.555",
+      toppingType: "1",
+      size: "L",
+    },
+    {
+      name: "PASTEL DE CHOCOLATE",
+      imageUrl: "https://axovia.mx/challenge/pasteldechocolate.jpeg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a lacus. Quisque in aliquet dui. Pellentesque mauris turpis, imperdiet ut viverra ut, porttitor sit amet felis. Ut viverra auctor quam in consectetur.",
+      price: "400.65232",
+      comboPrice: "1200.1",
+      toppingType: "2",
+      size: "L",
+    },
+    {
+      name: "PASTEL DE FRESAS",
+      imageUrl: "https://axovia.mx/challenge/fresas.jpeg",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a dictum lacus. Quisque in aliquet dui. Pellentesque mauris turpis, imperdiet ut viverra ut, porttitor sit amet felis. Ut viverra auctor quam in consectetur.",
+      price: "400.65232",
+      comboPrice: "1000",
+      toppingType: "2",
+      size: "M",
+    },
+    {
+      name: "Pastel de princesas",
+      imageUrl: "https://axovia.mx/challenge/princesas.jpeg",
+      description:
+        "Lorem ipsum dolor sit amet, adipiscing elit. Aenean a dictum lacus. Quisque in aliquet dui. Pellentesque mauris turpis, imperdiet ut viverra ut, porttitor sit amet felis. Ut viverra auctor quam in consectetur.",
+      price: "600.6",
+      comboPrice: "1500",
+      toppingType: "1",
+      size: "L",
+    },
+  ]);
 
   const getData = async () => {
     const dataob = await axios
@@ -99,9 +180,7 @@ const Section = () => {
       .then((result) =>
         result.data.replace(/(\r\n|\n|\r|\t)/gm, "").split("},")
       );
-    dataob.map((e, index) => {
-      console.log(e.replace('"{', ""));
-    });
+    dataob.map((e, index) => console.log(e.replace('"{', "")));
     console.log(dataob);
   };
 
@@ -118,7 +197,65 @@ const Section = () => {
         border: "1px solid rgba(231,228,230,255)",
       }}
     >
-      <FilterBar></FilterBar>
+      <div
+        className="container p-3"
+        style={{
+          backgroundColor: "#f9ecf7",
+          width: "100%",
+          borderRadius: "10px 10px 0 0",
+        }}
+      >
+        <div className="row">
+          <div className="col .placeholder">
+            <input
+              className="form-control"
+              placeholder=" Buscar"
+              style={{ fontFamily: "Arial, FontAwesome" }}
+            ></input>
+          </div>
+          <div className="col">
+            <select className="form-select">
+              <option selected>Precio</option>
+              <option> {"Entre 400"}</option>
+              <option> {"Más de 400"}</option>
+            </select>
+          </div>
+          <div className="col">
+            <select className="form-select">
+              <option selected>
+                <CurrencyDollar></CurrencyDollar>Precio
+              </option>
+              <option value="1">{"Menos de 500"}</option>
+              <option value="2">{"Entre 400 y 500"}</option>
+              <option value="3">{"Más de 500"}</option>
+            </select>
+          </div>
+          <div className="col">
+            <select className="form-select">
+              <option value="0">Fondeau</option>
+              <option value="1">Betun Italiano</option>
+              <option value="2">Chantilly</option>
+            </select>
+          </div>
+          <div className="col">
+            <select className="form-select">
+              <option value="S">Pequeño</option>
+              <option value="M">Mediano</option>
+              <option value="L">Grande</option>
+            </select>
+          </div>
+          <div className="col">
+            <select className="form-select">
+              <option selected>Ordenar Por</option>
+              <option value="0">Nombre</option>
+              <option value="1">Precio</option>
+              <option value="2">Precio Combo</option>
+              <option value="3">Topping</option>
+              <option value="4">Tamaño</option>
+            </select>
+          </div>
+        </div>
+      </div>
       {data.map((e, index) => (
         <Widget key={index} cake={e}></Widget>
       ))}
--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -31,10 +31,10 @@ const Widget = ({ cake }) => {
                 {cake.name.toLowerCase().replace(/^\w/, (c) => c.toUpperCase())}
               </span>
             </div>
-            <div className="col-2 cake">
+            <div className="col-2 cake p-3">
               <span>PASTEL</span>
             </div>
-            <div className="col-2 paq">
+            <div className="col-2 paq p-3 ">
               <span>PAQUETE</span>
             </div>
           </div>
@@ -44,6 +44,7 @@ const Widget = ({ cake }) => {
             <div className="col">
               <img
                 width={"70%"}
+                height={"200px"}
                 src={cake.imageUrl}
                 alt={cake.name}
                 className="rounded mx-auto d-block"
@@ -59,33 +60,49 @@ const Widget = ({ cake }) => {
                 </span>
               </div>
               <div className="col">
-                <div className="row">
-                  <span className="col-6">Tamaño:</span>
-                  <span className="col-6">{cake.size}</span>
+                <div className="row pt-4">
+                  <span className="col-3" style={{ fontWeight: 700 }}>
+                    Tamaño:
+                  </span>
+                  <span className="col-9" style={{ textAlign: "left" }}>
+                    {cake.size === "S"
+                      ? "Pequeño"
+                      : cake.size === "M"
+                      ? "Mediano"
+                      : "Grande"}
+                  </span>
                 </div>
                 <div className="row">
-                  <span className="col-6">Topping:</span>
-                  <span className="col-6">{cake.toppingType}</span>
+                  <span className="col-3" style={{ fontWeight: 700 }}>
+                    Topping:
+                  </span>
+                  <span className="col-9" style={{ textAlign: "left" }}>
+                    {cake.toppingType === "0"
+                      ? "Fondeau"
+                      : cake.toppingType === "1"
+                      ? "Betun Italiano"
+                      : "Chantilly"}
+                  </span>
                 </div>
               </div>
             </div>
             <div className="col">
               <div className="row">
-                <div className="col-6">
-                  <div className="row">
-                    <span>
-                      {cake.size === "S"
-                        ? "8-10 Personas"
-                        : cake.size === "M"
-                        ? " 10-15 Personas"
-                        : "15-25 Personas"}
-                    </span>
+                <div className="col-6 ">
+                  <div className="row justify-content-center bold">
+                    {cake.size === "S"
+                      ? "8-10 Personas"
+                      : cake.size === "M"
+                      ? " 10-15 Personas"
+                      : "15-25 Personas"}
                   </div>
-                  <div className="row">{`$ ${Number(cake.price).toFixed(
-                    2
-                  )}`}</div>
-                  <div className="row">*Solo Pastel</div>
-                  <div className="row">
+                  <div className="row justify-content-center bold price">{`$${Number(
+                    cake.price
+                  ).toFixed(2)}`}</div>
+                  <div className="row justify-content-center tip pb-3">
+                    *Solo Pastel
+                  </div>
+                  <div className="row justify-content-center">
                     <button
                       className="btn btn-success"
                       style={{
@@ -100,20 +117,16 @@ const Widget = ({ cake }) => {
                   </div>
                 </div>
                 <div className="col-6">
-                  <div className="row">
-                    <span>
-                      {cake.size === "S"
-                        ? "8-10 Personas"
-                        : cake.size === "M"
-                        ? " 10-15 Personas"
-                        : "15-25 Personas"}
-                    </span>
+                  <div className="row justify-content-center bold">
+                    Combo Fiesta
                   </div>
-                  <div className="row">{`$ ${Number(cake.comboPrice).toFixed(
-                    2
-                  )}`}</div>
-                  <div className="row">*Bebidas incluidas</div>
-                  <div className="row">
+                  <div className="row justify-content-center bold price">{`$${Number(
+                    cake.comboPrice
+                  ).toFixed(2)}`}</div>
+                  <div className="row justify-content-center tip pb-3">
+                    *Bebidas incluidas
+                  </div>
+                  <div className="row justify-content-center">
                     <button
                       className="btn btn-outline-success"
                       style={{
